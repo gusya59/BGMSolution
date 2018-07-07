3e8ae005d4de9c0023ecbe0884f394a3582b09b8
lina789 <alina.gusev@gmail.com>
2018-07-07 18:40:27 +0300

    //server connections

--- server/app.js
+++ server/app.js
@@ -1,31 +1,38 @@
-var createError = require('http-errors');
+
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+//var mongoose = require('mongoose');
 
+
+var app = express.Router();
+
+
+//routes
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
+//var apiRouter = require ('./routes/api')
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//DB configuration and connection
+// mongoose.connect(process.env.DB_URL,function(err){
+//   if(err) throw err;
+//   console.log("connected to data base");
+// }); 
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
+//app.use('./api', apiRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 // error handler
 app.use(function(err, req, res, next) {
