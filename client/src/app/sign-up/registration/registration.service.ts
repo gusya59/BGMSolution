cca03a61c1828af36ff40e4a512c1312cd55fa93
barsik22 <barsik22@gmail.com>
2018-07-17 23:56:28 +0300

    Register Page design //this change not working

--- client/src/app/sign-up/registration/registration.service.ts
+++ client/src/app/sign-up/registration/registration.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+
 
 @Injectable()
 export class CoinService {
@@ -8,7 +8,7 @@ export class CoinService {
   constructor(private http: HttpClient) { }
 
   addCoin(firstName, lastName, email, password) {
-    const uri = 'http://localhost:4200/signup/registration';
+    const uri = 'http://localhost:4200/signup/';
     const obj = {
       firstName: firstName,
       lastName: lastName,
