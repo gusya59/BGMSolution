cc7e45c97ccc1cd51a1d193adde4dbee0cb0bed5
barsik22 <barsik22@gmail.com>
2018-08-12 23:17:42 +0300

    jwt fixed. header fixed. can issue request for user and for admin

--- client/src/app/service/routes.ts
+++ client/src/app/service/routes.ts
@@ -1,4 +1,5 @@
 import { AuthGuard } from './auth.guard';
+import {RoleGuardServiceService as RoleGuard} from './role-guard-service.service'
 import { SysinfoComponent } from './../admin/sysinfo/sysinfo.component';
 import { WeightsComponent } from './../admin/weights/weights.component';
 import { ReportsComponent } from './../admin/reports/reports.component';
@@ -81,42 +82,49 @@ export const appRoutes: Routes = [
     {
       path: 'admin',
       component: AdminComponent,
-      data: {title: 'admin'},
+      canActivate: [RoleGuard],
+      data: {title: 'admin'}
     },
     //admin admins list comonent
     {
       path: 'admin/admins',
       component: AdminsComponent,
+      canActivate: [RoleGuard],
       data: {title: 'admin/admins'} 
     },
     //admin Users list comonent
     {
       path: 'admin/users',
       component: UsersComponent,
+      canActivate: [RoleGuard],
       data: {title: 'admin/users'} 
     },
     //admin questions list comonent
     {
       path: 'admin/questions',
       component: AdminQuestionsComponent,
+      canActivate: [RoleGuard],
       data: {title: 'admin/questions'} 
     },
     //admin questions list comonent
     {
       path: 'admin/reports',
       component: ReportsComponent,
+      canActivate: [RoleGuard],
       data: {title: 'admin/reports'} 
     },
     //admin questions list comonent
     {
       path: 'admin/weights',
       component: WeightsComponent,
+      canActivate: [RoleGuard],
       data: {title: 'admin/weights'} 
     },  
     //admin questions list comonent
     {
       path: 'admin/sysinfo',
       component: SysinfoComponent,
+      canActivate: [RoleGuard],
       data: {title: 'admin/sysinfo'} 
     },            
     //Login for our user routing removed from this version (read component instruction)
