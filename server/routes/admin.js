1aee396c13d3fc69dc31da3947a68fe0fe808c33
lina789 <alina.gusev@gmail.com>
2018-08-14 20:25:25 +0300

    /admin/ info fix. check the token if it is admin

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,35 +6,44 @@ var registrationSchema = require('../models/Registration.js');
 var verFuncs = require('../utils/verificationFunctions.js')
 
 //registration
-router.post('/info', async function (req, res) {
-    console.log("the input is: " + req.body);
-
-    //the amount of regular users
+router.post('/info',verFuncs.getTokenFromHeaders, async function (req, res) {
+  console.log("the input is: " + req.body);
+  var verifyToken = verFuncs.verifyToken(req.token, jwt);
+  var check =verFuncs.decodeisAdmin(req.token, jwt);
+  //check if the loken valid and if the user has admin permissions
+  if(verifyToken && check){
+      //the amount of regular users
     var regUserAmount = await registrationSchema.countRegularUsers();
-      console.log("the regular user's amount is "+regUserAmount);
+    console.log("the regular user's amount is "+regUserAmount);
     //the amount of admin users
     var adminUserAmount = await registrationSchema.countAdminUsers();
     console.log("the admin user's amount is "+adminUserAmount);
     res.status(200).send({ success: true, regUserAmount: regUserAmount, adminUserAmount: adminUserAmount }) 
+  }
+ 
+    res.status(200).send({ success: false, message: "session is expired" })
   });
   
-
+ //find user by permission. token verification
+ //input: token, permission type: true for admin, false for regular user
+ //outpit: object with all the relevant users
   router.post('/admins',verFuncs.getTokenFromHeaders, async function (req, res) {
-    console.log("the input is: " + req.body);
     var verifyToken = verFuncs.verifyToken(req.token, jwt);
     if(verifyToken){
-      //find all users with admin permissions
-      var adminUsers = await registrationSchema.findAllByPermission(true);
+      //find all users with specific permissions.
+      var adminUsers = await registrationSchema.findAllByPermission(req.body.isAdminPer);
       console.log(adminUsers);
       res.status(200).send({ success: true,adminUsers: adminUsers}) 
     }
     else{
-      res.status(403).send({ success: false, message: "session is expired" })
+      res.status(200).send({ success: false, message: "session is expired" })
     }
 
     });
 
-  //delete user. demote admin button
+  //delete user.
+  //input: email of the user that need to be deleted
+  //output: true on success, else false
   router.post('/admins/remove', async function (req, res) {
     console.log("the input is: " + req.body.email);
     var deleteUser = await registrationSchema.deleteUser(req.body.email);
@@ -47,6 +56,8 @@ router.post('/info', async function (req, res) {
   });
 
     //change permissions. will be used for /admins and for /users routes
+    //input: email in order to find the user in the db, permission type: true for admin, false for regular user
+    //output: true on success, else false
     router.post('/admins/changePermissions', async function (req, res) {
       var updatedUser = await registrationSchema.findByEmailAndUpdate(req.body.email,{isAdmin:req.body.isAdminPer});
       if(updatedUser){
