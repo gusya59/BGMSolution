2448652cb5d83d79bf4cfc034058a5a754ca4ac6
barsik22 <barsik22@gmail.com>
2018-08-19 15:18:37 +0300

    user settings preview

--- client/src/app/user/preview/preview.component.ts
+++ client/src/app/user/preview/preview.component.ts
@@ -1,6 +1,7 @@
 import { UserPreviewService } from './../../service/user-preview.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { forEach } from '../../../../node_modules/@angular/router/src/utils/collection';
 
 
 @Component({
@@ -10,111 +11,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviewComponent implements OnInit {
 
-  // Define % of our table bars
-  widthFacebook: string ="0%";
-  widthInstagram: string ="0%";
-  widthGoogleP : string ="0%";
-  widthGoogleAdWords: string ="0%";
-  widthGoogle: string ="0%";
-  widthGoogleMybuissness: string ="0%";
-  widthTwiiter: string ="0%";
+
+
 
   // Define the original budget per line
   
-  budgetFacebook: number =0;
-  budgetInstagram: number =0;
-  budgethGoogleP : number =0;
-  budgetGoogleAdWords: number =0;
-  budgetGoogle: number =0;
-  budgetGoogleMybuissness: number =0;
-  budgetTwiiter: number =0;
+  platforms:[
+    {
+      platform_id: string,
+      platform_name: string,
+      platform_budget: number
+    }
+  ];
+
+  //define bar width
+
+  widthBar: string [];
+
+  //define budget total
   budgetTotal: number =0;
 
-  //array of budgets
-  budget = [this.budgetFacebook, this.budgetInstagram, this.budgethGoogleP, this.budgetGoogleAdWords, this.budgetGoogle, this.budgetGoogleMybuissness,
-  this.budgetTwiiter];
-
-  //array of width
-  width = [this.widthFacebook, this.widthInstagram, this.widthGoogleP, this.widthGoogleAdWords, this.widthGoogle, this.widthGoogleMybuissness,
-    this.widthGoogleMybuissness];
+  //define color array
+  colorArray: string[];
   
   
   constructor(private userPreview: UserPreviewService) { }
 
   ngOnInit() {
-    //no server
-    //on page load show bars with defalt 0
-    this.widthFacebook;
-    this.widthInstagram;
-    this.widthGoogleP;
-    this.widthGoogleAdWords;
-    this.widthGoogle;
-    this.widthGoogleMybuissness;
-    this.widthTwiiter;
-
-
-
-    //on page load show budget
-    this.budgetFacebook;
-    this.budgetInstagram;
-    this.budgethGoogleP ;
-    this.budgetGoogleAdWords;
-    this.budgetGoogle;
-    this.budgetGoogleMybuissness;
-    this.budgetTwiiter;
-
-    //server up get request for data
-    this.userPreview.getPreview().subscribe(Data =>{
-      
-    this.budgetFacebook = Data.budgetFacebook;
-    this.budgetInstagram = Data.budgetInstagram;
-    this.budgethGoogleP = Data.budgethGoogleP;
-    this.budgetGoogleAdWords = Data.budgetGoogleAdWords;
-    this.budgetGoogle = Data.budgetGoogle;
-    this.budgetGoogleMybuissness = Data.budgetGoogleMybuissness;
-    this.budgetTwiiter = Data.budgetTwiiter;
     
-  //store data in array of budgets
-  this.budget = [this.budgetFacebook, this.budgetInstagram, this.budgethGoogleP, this.budgetGoogleAdWords, this.budgetGoogle, this.budgetGoogleMybuissness,
-    this.budgetTwiiter];
-      
-    this.countTotal();
-    console.log(Data);
-    console.log(this.budget);
-    this.calcPercentage();
-  });
-    
+    //Budgets on page load
+    this.getBudgets();
 
-
+    //calc total budget
+    this.barSettings();
   }
+
+  //set bar settings
+  barSettings(){
+    //define empty array
+  var arr = new Array();
+    //new array to build
+    var widhtB = new Array();
+
+    // loop and set push to array
+
+  this.platforms.forEach((p) => {
+    widhtB.push(+((p.platform_budget / this.budgetTotal) *100).toFixed(2) +"%");
+
+    //puch to arr
+    arr.push(this.getRandomColor());
+
+    console.log(arr);
+    //set to our color array
+    this.colorArray = arr; 
+  }
+  )
+  //set width bar with our width
+  this.widthBar = widhtB;
+  // console.log(this.widthBar)
+};
+
+  
   // functions will intercept server budgets and will generate table
 
   countTotal(){
-
-    //loop to sum on budget
-    for(let b in this.budget){
-      this.budgetTotal += this.budget[b];
+    
+    //loop and calculate budget Total
+    this.platforms.forEach((p)=>{
+      this.budgetTotal = this.budgetTotal + p.platform_budget;
+      // console.log(p.platform_budget)
     }
-
+    
+  )
+  // console.log("total: " + this.budgetTotal)
   }
 
-  calcPercentage(){
-    //loop for width values and calc the %
-    for(let b in this.width){
-      this.width[b] = +((this.budget[b] / this.budgetTotal) *100).toFixed(2) +"%"
-      console.log(this.width[b])
-    }
-    //set width values to display
-  this.widthFacebook = this.width[0];
-  this.widthInstagram = this.width[1];
-  this.widthGoogleP = this.width[2];
-  this.widthGoogleAdWords = this.width[3];
-  this.widthGoogle = this.width[4];
-  this.widthGoogleMybuissness = this.width[5];
-  this.widthTwiiter = this.width[6];
-    
-  } 
+  getBudgets(){
+    //server up get request for data
+    this.userPreview.getPreview().subscribe(Data =>{
+      this.platforms = Data.platforms;
+      
+  
+    this.countTotal();
+
+    // console.log(this.budget);
 
 
+  });
+  }
+
+  // randomize color
+
+  getRandomColor() {
+    var color = Math.floor(0x1000000 * Math.random()).toString(16);
+    return '#' + ('000000' + color).slice(-6);
+  }
 
 }
