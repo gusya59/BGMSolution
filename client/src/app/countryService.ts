86790bb594d1ab5cca7e6c59551257375ac72d6a
lina789 <alina.gusev@gmail.com>
2018-09-13 10:04:07 +0300

    added cities

--- client/src/app/countryService.ts
+++ client/src/app/countryService.ts
@@ -15,14 +15,28 @@ export class countryService {
     //array of citys according to countrys
     getCities() {
         return [
-            new city(1, 1, 'Haifa'),
-            new city(2, 1, 'Tel-Aviv'),
-            new city(3, 1, 'Jerusalem'),
-            new city(4, 1, 'Nazareth-illit'),
+            new city(1, 1, 'Acre'),
+            new city(2, 1, 'Afula'),
+            new city(3, 1, 'Ashdod'),
+            new city(4, 1, 'Ashkelon'),
             new city(5, 1, 'Afula'),
-            new city(6, 1, 'Be er - sheva'),
+            new city(6, 1, 'Beersheba'),
+            new city(7, 1, 'Beit Shean'),
+            new city(8, 1, 'Eilat'),
+            new city(9, 1, 'Hadera'),
+            new city(10, 1, 'Haifa'),
+            new city(11, 1, 'Jerusalem'),
+            new city(12, 1, 'Migdal HaEmek'),
+            new city(13, 1, 'Nazareth'),
+            new city(14, 1, 'Nazareth Illit'),
+            new city(15, 1, 'Netanya'),
+            new city(16, 1, 'Rishon LeZion'),
+            new city(17, 1, 'Tel Aviv'),
+            new city(18, 1, 'Other'),
+
             new city(1, 2, 'Moscow'),
             new city(2, 2, 'Saint-Petersburg'),
+            new city(2, 3, 'Other'),
             ];
         }
 }
